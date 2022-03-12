62654c362b62169d0ee5fbf662b50989308f55b8
hugosenari <hugosenari@gmail.com>
2022-03-12 02:39:57 -0300

    chore: re index desim

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A lightweight discrete event simulator'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-desim-master.flake = false;
+  inputs.src-desim-master.owner = "";
+  inputs.src-desim-master.ref   = "refs/heads/master";
+  inputs.src-desim-master.repo  = "raw.githubusercontent.com";
+  inputs.src-desim-master.type  = "other";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-desim-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-desim-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
